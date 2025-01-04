2b7d88891a3fbcb678099f8543d817069798cd64
Abubakr <abubakr.hemed@gmail.com>
2025-01-04 16:43:34 -0500

    upadted navbar and completed hero

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
 const hamburgerBtn = document.querySelector("#NavBtn");
 const slideNav = document.querySelector("#slideNav");
 
-// State to track whether the menu is open or closed
 let isMenuOpen = false;
 
-// Add click event listener to the button
 hamburgerBtn.addEventListener("click", () => {
     if (isMenuOpen) {
-        // Slide out the menu (hide it)
-        slideNav.style.transform = "translateX(-100%)";
-        slideNav.style.transition = "transform 2s ease";
+        slideNav.classList.remove("active");
         hamburgerBtn.querySelector("img").src = "./Assets/hamburgerIcon.png";
         isMenuOpen = false;
     } else {
-        // Slide in the menu (show it)
-        slideNav.style.transform = "translateX(0)";
-        slideNav.style.transition = "transform 2s ease";
+        slideNav.classList.add("active");
         hamburgerBtn.querySelector("img").src = "./Assets/closeIcon.png";
         isMenuOpen = true;
     }
