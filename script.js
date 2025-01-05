20d89e54bb47bdea5f87e7202347b41e38f12023
Abubakr <abubakr.hemed@gmail.com>
2025-01-05 14:11:12 -0500

    made Nav fixed and fixed anchor scrolling

--- script.js
+++ script.js
@@ -39,5 +39,25 @@ navLinks.forEach(link => {
     });
 });
 
+
+// Adjust scrolling to account for fixed navbar
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute("href").substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            const offsetTop = targetElement.offsetTop - document.querySelector("nav").offsetHeight;
+
+            window.scrollTo({
+                top: offsetTop,
+                behavior: "smooth",
+            });
+        }
+    });
+});
+
 navBtn();
 slidebtn();
